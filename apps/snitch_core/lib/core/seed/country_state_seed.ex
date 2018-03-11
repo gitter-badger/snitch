511678d0327229527e7f8334d3d28d84065c2113
Gopal Shimpi <gopalshimpi@gmail.com>
2018-03-11 15:29:24 +0530

    Country & state seed refactored.
    1. Used insert_all insted of insert to speed up the seeding task.
    2. Avoided duplication of seed when, seed ran more than once.

--- apps/snitch_core/lib/core/seed/country_state_seed.ex
+++ apps/snitch_core/lib/core/seed/country_state_seed.ex
@@ -6,48 +6,62 @@ defmodule Snitch.Seed.CountryState do
 
   alias Worldly.Country, as: WorldCountry
   alias Worldly.Region, as: WorldRegion
-  alias Snitch.Data.Schema.Country
   alias Snitch.Repo
 
   def seed_countries_and_states! do
-    Enum.each(WorldCountry.all(), fn country -> seed_country_data(country) end)
+    insert_countries() 
   end
 
-  def seed_country_data(country) do
-    change = Country.changeset(%Country{}, to_param(country))
-    inserted_country = Repo.insert!(change)
+  def insert_countries do
+    wc = WorldCountry.all()
+    all_countries = Enum.map(wc, fn country -> to_param(country) end)
+    {_ ,countries_ids} = Repo.insert_all("snitch_countries", 
+                                        all_countries, 
+                                        on_conflict: :nothing, 
+                                        returning: [:id])    
+    country_id = Enum.map(countries_ids, fn (x) -> x.id end)
+    
+    wc 
+    |> Enum.map(&WorldRegion.regions_for/1)
+    |> Enum.zip(country_id)
+    |> Enum.map(fn {statelist, id} -> 
+                    Enum.map(statelist, fn state -> 
+                          to_param(state, id) end) end)
+    |> List.flatten
+    |> insert_and_map_states  
 
-    if country.has_regions do
-      Enum.each(WorldRegion.regions_for(country), fn region ->
-        seed_state_data(inserted_country, region)
-      end)
-    end
   end
 
-  def seed_state_data(country, state) do
-    country
-    |> Ecto.build_assoc(:states, to_param(state))
-    |> Repo.insert!()
+  def insert_and_map_states(states) do
+    Repo.insert_all("snitch_states", states, on_conflict: :nothing)    
   end
-
+  
   defp to_param(%WorldCountry{
-         name: name,
-         alpha_2_code: iso,
-         alpha_3_code: iso3,
-         numeric_code: numcode,
-         has_regions: has_regions
-       }) do
+      name: name,
+      alpha_2_code: iso,
+      alpha_3_code: iso3,
+      numeric_code: numcode,
+      has_regions: has_regions
+    }) do
     %{
       name: name,
       iso: iso,
       iso3: iso3,
       numcode: numcode,
       states_required: has_regions,
-      iso_name: String.upcase(name)
+      iso_name: String.upcase(name),
+      inserted_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()  
     }
   end
 
-  defp to_param(%WorldRegion{code: abbr, name: name}) do
-    %{abbr: to_string(abbr), name: to_string(name)}
+  defp to_param(%WorldRegion{code: abbr, name: name}, id) do
+    %{
+      abbr: to_string(abbr), 
+      name: to_string(name),
+      country_id: id,
+      inserted_at: Ecto.DateTime.utc(),
+      updated_at: Ecto.DateTime.utc()  
+    }
   end
 end
